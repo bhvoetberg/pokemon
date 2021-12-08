8a86e7cb5005a65c4b5200f86126940a4cada9ea
bhvoetberg <b.h.voetberg@gmail.com>
2021-12-08 14:21:44 +0100

    1 Pokemon met gegevens op pagina

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
+import Pokemon from "./components/Pokemon";
 
 function App() {
-  return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
-  );
+    const [pokemonData, setPokemonData] = useState('');
+    const [endPoint, setEndPoint] = useState('https://pokeapi.co/api/v2/pokemon/spearow');
+
+    useEffect(() => {
+        async function getData() {
+            try {
+                const result = await axios.get(endPoint);
+                setPokemonData(result.data);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        getData();
+        console.log(pokemonData);
+    }, [])
+
+    return (
+        <div>
+            {pokemonData && <>
+                <h2>{pokemonData.name}</h2>
+                <img src={pokemonData.sprites.front_default} alt={pokemonData.name}/>
+                <h3><strong>Moves: </strong> {pokemonData.moves.length}</h3>
+                <h3><strong>Weight: </strong> {pokemonData.weight}</h3>
+                <h3><strong>Abilities: </strong></h3>
+
+                {pokemonData.abilities.map((pokemon) => {
+                    return <li> {pokemon.ability.name}</li>
+                    })}
+            </>
+            }
+        </div>
+    );
+    // Pokemon(endPoint={en})
+
 }
 
 export default App;
---
+++ src/components/Pokemon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Pokemon = ({endPoint}) => {
+    // const [pokemonData, setPokemonData] = useState('');
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Pokemon;
