7a014261a27d8d545b0ab388e2850ad21bef74f2
bhvoetberg <b.h.voetberg@gmail.com>
2021-12-08 14:45:03 +0100

    Pokemon component werkend

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './App.css';
 import Pokemon from "./components/Pokemon";
 
+
 function App() {
     const [pokemonData, setPokemonData] = useState('');
     const [endPoint, setEndPoint] = useState('https://pokeapi.co/api/v2/pokemon/spearow');
@@ -22,22 +23,9 @@ function App() {
 
     return (
         <div>
-            {pokemonData && <>
-                <h2>{pokemonData.name}</h2>
-                <img src={pokemonData.sprites.front_default} alt={pokemonData.name}/>
-                <h3><strong>Moves: </strong> {pokemonData.moves.length}</h3>
-                <h3><strong>Weight: </strong> {pokemonData.weight}</h3>
-                <h3><strong>Abilities: </strong></h3>
-
-                {pokemonData.abilities.map((pokemon) => {
-                    return <li> {pokemon.ability.name}</li>
-                    })}
-            </>
-            }
+            <Pokemon pokemonData={pokemonData}/>
         </div>
     );
-    // Pokemon(endPoint={en})
-
 }
 
 export default App;
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const Pokemon = ({endPoint}) => {
+const Pokemon = ({pokemonData}) => {
     // const [pokemonData, setPokemonData] = useState('');
 
     return (
         <div>
+            {pokemonData && <>
+                <h2>{pokemonData.name}</h2>
+                <img src={pokemonData.sprites.front_default} alt={pokemonData.name}/>
+                <h3><strong>Moves: </strong> {pokemonData.moves.length}</h3>
+                <h3><strong>Weight: </strong> {pokemonData.weight}</h3>
+                <h3><strong>Abilities: </strong></h3>
 
+                {pokemonData.abilities.map((pokemon) => {
+                    return <li> {pokemon.ability.name}</li>
+                })}
+            </>
+            }
         </div>
     );
 };
